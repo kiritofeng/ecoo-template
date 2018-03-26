9f38ae3ebc0516a9e3fb3248f1829943b31c0c04
Kirito Feng <kiritofeng@users.noreply.github.com>
2018-03-26 09:16:36 -0400

    Create manacher.h

---
+++ manacher.h
@@ -0,0 +1,33 @@
+#include<vector>
+inline std::vector<int> manacher(char *S){
+  int len=strlen(S);
+  char *T=new char[len*2+1];
+  memset(T,'#',sizeof T);
+  for(int i=0;i<len;++i)
+    T[i<<1|1]=S[i];
+  std::vector<int>res=vector<int>(len*2+1,0);
+  int rht=0,mir=0;
+  for(int i=1;i<=len*2;++i){
+    if(i>rht) res[i]=1;
+    else res[i]=std::min(res[2*mir-rht],rht-i);
+    while(i>=res[i]&&T[i-res[i]]==T[i+res[i]]) ++res[i];
+    if(i+res[i]>rht) mir=i,rht=i+res[i];
+  }
+  return res;
+}
+inline std::vector<int> manacher(const std::string &S){
+  int len=S.length();
+  char *T=new char[len*2+1];
+  memset(T,'#',sizeof T);
+  for(int i=0;i<len;++i)
+    T[i<<1|1]=S[i];
+  std::vector<int>res=vector<int>(len*2+1,0);
+  int rht=0,mir=0;
+  for(int i=1;i<=len*2;++i){
+    if(i>rht) res[i]=1;
+    else res[i]=std::min(res[2*mir-rht],rht-i);
+    while(i>=res[i]&&T[i-res[i]]==T[i+res[i]]) ++res[i];
+    if(i+res[i]>rht) mir=i,rht=i+res[i];
+  }
+  return res;
+}
