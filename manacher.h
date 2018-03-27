e47f653a27c43a1addb0a866302ec022f02fbf35
Kirito Feng <kiritofeng@users.noreply.github.com>
2018-03-27 12:12:26 -0400

    Bug fixes

--- manacher.h
+++ manacher.h
@@ -1,4 +1,3 @@
-#include<vector>
 inline std::vector<int> manacher(char *S){
   int len=strlen(S);
   char *T=new char[len*2+1];
@@ -9,25 +8,26 @@ inline std::vector<int> manacher(char *S){
   int rht=0,mir=0;
   for(int i=1;i<=len*2;++i){
     if(i>rht) res[i]=1;
-    else res[i]=std::min(res[2*mir-rht],rht-i);
+    else res[i]=std::min(res[2*mir-i],rht-i);
     while(i>=res[i]&&T[i-res[i]]==T[i+res[i]]) ++res[i];
     if(i+res[i]>rht) mir=i,rht=i+res[i];
   }
   return res;
 }
-inline std::vector<int> manacher(const std::string &S){
-  int len=S.length();
-  char *T=new char[len*2+1];
-  memset(T,'#',sizeof T);
-  for(int i=0;i<len;++i)
-    T[i<<1|1]=S[i];
-  std::vector<int>res=vector<int>(len*2+1,0);
-  int rht=0,mir=0;
-  for(int i=1;i<=len*2;++i){
-    if(i>rht) res[i]=1;
-    else res[i]=std::min(res[2*mir-rht],rht-i);
-    while(i>=res[i]&&T[i-res[i]]==T[i+res[i]]) ++res[i];
-    if(i+res[i]>rht) mir=i,rht=i+res[i];
-  }
-  return res;
+
+inline vector<int> manacher(const string &S){
+	int len=S.length();
+	char *T=new char[len*2+1];
+	memset(T,'#',(len*2+1)*sizeof(char));
+	for(int i=0;i<len;++i)
+		T[i<<1|1]=S[i];
+	vector<int>res=vector<int>(len*2+1,0);
+	int rht=0,mir=0;
+	for(int i=1;i<=len*2;++i){
+		if(i>=rht) res[i]=1;
+		else res[i]=min(res[2*mir-i],rht-i);
+		while(i>=res[i]&&T[i-res[i]]==T[i+res[i]]) ++res[i];
+		if(i+res[i]>rht) mir=i,rht=i+res[i];
+	}
+	return res;
 }
