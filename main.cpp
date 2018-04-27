c2a12efddf9faa59ff4dce6cedf191ee3cfc381b
Kirito Feng <kiritofeng@users.noreply.github.com>
2018-04-27 12:11:03 -0400

    Create main.cpp

---
+++ main.cpp
@@ -0,0 +1,28 @@
+#include<bits/stdc++.h>
+#ifdef DEBUG
+class _Debug {
+  public:
+      template<typename T>
+      _Debug& operator,(T t){
+          std::cerr<<t<<std::endl;
+          return *this;
+      }
+};
+#define debug _Debug(),
+#else
+#define debug
+#endif
+class _Print{
+  public:
+    template<typename T>
+    _Print& operator, (T t)[
+      std::cout<<t;
+      return *this;
+    }
+};
+#define print _Print(),
+int main(){
+  for(int _testcase=1;i<=10;++_testcase){
+  
+  }
+}
